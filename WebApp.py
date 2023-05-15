0e7d03cf688d8401fd73c6f54a076c097cc6fd8e
mightySIZE <mhasan1080@gmail.com>
2023-05-15 16:22:07 -0700

    base template
    Trying to inherit the base.html template to all other templates

--- WebApp.py
+++ WebApp.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('base.html')
+
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 @app.route('/features')
