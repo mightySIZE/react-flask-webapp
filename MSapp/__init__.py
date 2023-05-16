23c211af0b6311b3b265460d4655108319027b0c
mightySIZE <mhasan1080@gmail.com>
2023-05-16 15:55:16 -0700

    created a user model
    connects with great model

--- MSapp/__init__.py
+++ MSapp/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__) # this line will create the flask object
+app.app_context().push() # this line will create the application context, which is needed for the db object because it is not created with the app object in this file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/mo/Desktop/current project/MSapp/instance/MS.db' # this line will connect flask with the database, also /// means relative path, //// means absolute path
 db = SQLAlchemy(app) # this line will create the database object
 migrate = Migrate(app, db) # this line will create the migration object
--- MSapp/models.py
+++ MSapp/models.py
@@ -1,6 +1,12 @@
 from MSapp import db
 
-# this class will create the table in the database
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(60), nullable=False)
+    great = db.relationship('Great', backref='great_user', lazy=True)
+
 class Great(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
@@ -9,6 +15,7 @@ class Great(db.Model):
     death = db.Column(db.String(80), unique=False, nullable=True)
     age = db.Column(db.Integer, unique=False, nullable=False)
     url = db.Column(db.String(200), unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return '<Great %r>' % self.name
