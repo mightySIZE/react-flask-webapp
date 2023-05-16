364daaf94de6cc5cd97ef5608cf61ccda84382db
mightySIZE <mhasan1080@gmail.com>
2023-05-15 20:29:58 -0700

    Checkpoint #1
    5 templates that inherit from base.html template. No additional functionalities are present

--- WebApp.py
+++ WebApp.py
@@ -6,10 +6,6 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('base.html')
-
-@app.route('/home')
 def home():
     return render_template('home.html')
 
