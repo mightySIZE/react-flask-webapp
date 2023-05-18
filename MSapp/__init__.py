3e4130707018ee32eafa2afb4d587f1355483d87
mightySIZE <mhasan1080@gmail.com>
2023-05-17 18:18:45 -0700

    configured the cookie duration for remmeber me
    although "REMEMBER_COOKIE_DURATION'" didn't work so i had to use "PERMANENT_SESSION_LIFETIME"

--- MSapp/__init__.py
+++ MSapp/__init__.py
@@ -3,11 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from datetime import timedelta
 
 app = Flask(__name__) # this line will create the flask object
 app.app_context().push() # this line will create the application context, which is needed for the db object because it is not created with the app object in this file
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280bc245' # this is the secret key for the form
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/mo/Desktop/current project/MSapp/instance/MS.db' # this line will connect flask with the database, also /// means relative path, //// means absolute path
+app.config['REMEMBER_COOKIE_DURATION'] = timedelta(seconds=3600) # this line will set the remember me cookie to expire after 30 seconds
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=3600) # this line will set the session to expire after 30 seconds
 db = SQLAlchemy(app) # this line will create the database object
 migrate = Migrate(app, db) # this line will create the migration object
 bcrypt = Bcrypt(app) # this line will create the bcrypt object
--- MSapp/routes.py
+++ MSapp/routes.py
@@ -51,7 +51,7 @@ def login():
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data): # this line will check if we have a user with the username that the user entered and if the password that the user entered is correct
             if form.remember.data:
                 login_user(attempted_user, remember=True)
-                flash(f'Success! You are logged in as: {attempted_user.username} and you are remembered!', category='success')
+                flash(f'Success! You are logged in as: {attempted_user.username}, and will be remembered for 1 hour', category='success')
                 return redirect(url_for('home'))
             
             login_user(attempted_user)
